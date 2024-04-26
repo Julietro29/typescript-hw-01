78666b103a6b0cb21b49d83176146eea5b7870f3
Julietro29 <mousemiu1@gmail.com>
2024-04-27 01:13:43 +0300

    add all tasks

---
+++ src/generics/1.ts
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+async function fetchData<T>(url: string): Promise<T> {
+    try {
+        const response = await axios.get(url);
+        return response.data;
+    } catch (error) {
+        throw new Error(`Error fetching from ${url}: ${error}`);
+    }
+}
+
+interface IFetchedTodos{
+    userId: number;
+    id: number;
+    title: string;
+    completed: boolean
+}
+
+fetchData<IFetchedTodos[]>('https://jsonplaceholder.typicode.com/todos')
+    .then((results) => {
+        console.log('Received data:', results);
+})
+    .catch((error) => {
+        console.error('Error fetching data:', error.message);
+});
